2d8739e120fc102c791311e7fdaf69fe3d4a88e2
tlgenzer <tlgenzer@gmail.com>
2019-11-22 13:36:51 -0600

    Flag

---
+++ Project/Project/Flag.java
@@ -0,0 +1,37 @@
+import mayflower.*; //tell Java we will be using the Mayflower API
+
+public class Flag extends Actor
+{
+    private int playFlag = 0;   
+    //This is the constructor. It is called exactly once, when the Mayrio object is first created
+    public Flag() 
+    {
+      setImage("images/Flag.png");
+    }
+  
+    //This method is called once every frame (about 60 times per second)
+    public void act()
+    {
+      int oldX = getX();
+      int oldY = getY();
+      
+      if(isTouching(Player.class))
+      {
+        setLocation( getX(), getY()+1 );
+        playFlag++;
+          
+      }
+      
+      if(oldY>650)
+      {
+        setLocation( 1000, 1000 );
+        
+          
+      }
+      if(playFlag==1)
+      {
+       Mayflower.playMusic("sound/MarioWin.mp3"); 
+        
+      }
+    }
+}
